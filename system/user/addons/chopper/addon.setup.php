e7c3d64a704dc95b595783d7dfff3d9d47f31487
BBDOKC <bmallett@gmail.com>
2018-01-04 13:20:05 -0600

    Update addon.setup.php

--- system/user/addons/chopper/addon.setup.php
+++ system/user/addons/chopper/addon.setup.php
@@ -4,9 +4,9 @@ return array(
     'author' => 'BBDOKC',
     'author_url' => 'http://bbdokc.com',
     'description' => 'Allows you to create excerpts of your entries by removing HTML tags and limited the excerpt by character count, word count or a specific marker you insert into your content. Thanks to Brett DeWoody for providing Hacksaw. I needed one for EE3 and created the same functionality from it since it was let go.',
-    'docs_url' => 'https://github.com/BlackBeltDesigns/bbd_chopper.ee_addon',
+    //'docs_url' => 'https://github.com/BlackBeltDesigns/bbd_chopper.ee_addon',
     'name' => 'BBD Chopper',
     'settings_exist' => false,
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'namespace'   => 'BBDOKC\Chopper'
 );
